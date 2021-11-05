fae9c90465c61817bd98dfee4720e323e4e8ea91
alvin pasmah <78840877+pahlalvin@users.noreply.github.com>
2021-11-05 15:21:11 +0700

    Pengumpulan minggu4
    fungsi CRUD

---
+++ index.php
@@ -0,0 +1,44 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRAKTIKUM ALVIN PASMAH</title>
+</head>
+<body>
+    <hr>
+    <section class="">
+        <div class="row">
+            <div class="col-6">
+                <div id="read_data"></div>
+            </div>
+            <div class="col-6">
+                <h2>FORM DATA MAHASISWA</h2>
+                <form action="" method="POST" id="form_mahasiswa">
+                    <label for="id" id="id_label" hidden >No :</label>
+                    <input type="text" name="id" id="id" hidden readonly> <br> <br>
+                    <label for="">NIM : </label>
+                    <input type="text" name="nim" id=""> <br> <br>
+                    <label for="">Nama : </label>
+                    <input type="text" name="nama" id=""> <br> <br>
+                    <input type="radio"
+                    <label for="">Prodi : </label>
+                        <input type="radio" name="prodi" value="1"/>1. Elektro<br />
+                        <input type="radio" name="prodi" value="2" checked/>2. Informatika<br />
+                        <input type="radio" name="prodi" value="3"/>3. Fisika<br />
+                        <input type="radio" name="prodi" value="4"/>4. Telekomunikasi<br />
+                        <input type="radio" name="prodi" value="5"/>5. Mesin<br />
+                    </select> <br> <br>
+                    <label for="">Angkatan : </label>
+                    <input type="text" name="angkatan" id=""> <br> <br>
+                    <button class="btn btn-success" id="submit">Tambahkan</button>
+                    <button class="btn btn-primary" id="update" hidden>Update</button>
+                    <button class="btn btn-danger" id="cancel" hidden>Cancel Update</button>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+<script src="jquery-3.5.1.min.js"></script>
+<script src="index.js"></script>
+</html>
