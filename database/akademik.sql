b9ffa82d1ec524eff245d3e5e27b63bacaab833e
alvin pasmah <78840877+pahlalvin@users.noreply.github.com>
2021-11-26 23:16:01 +0700

    mengumpulkan tugas minggu 7
    praktikum minggu 7 website sederhana

---
+++ database/akademik.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Nov 2021 pada 16.45
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `Nim` int(10) NOT NULL,
+  `Nama_Mhs` varchar(50) NOT NULL,
+  `Jenis_Kelamin` varchar(50) NOT NULL,
+  `Program_Studi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`Nim`, `Nama_Mhs`, `Jenis_Kelamin`, `Program_Studi`) VALUES
+(118130021, 'Lori Kogawa', 'Perempuan', 'teknik mesin'),
+(118130026, 'Luna Maya', 'Perempuan', 'Akting'),
+(118130027, 'Kimi Hime', 'Perempuan', 'Farmasi'),
+(118130028, 'uchiha sasuke', 'Laki-Laki', 'Kedokteran'),
+(118130029, 'Monkey D luffy', 'Perempuan', 'Farmasi'),
+(118130232, 'naruto', 'Laki-Laki', 'Teknik Sipil'),
+(1803040005, 'Kimi Hime', 'Perempuan', 'Farmasi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`Nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ home.php
@@ -0,0 +1,5 @@
+<center><br><br><br><br><br><br><br><br>
+<img src="assets/images/Itera-logo.png" width="200px height="200px" /> <br>
+<font Size="6" face="Helvetica">Praktikum Pemweb RD</font> <br>
+<font Size="6">Institut Teknologi Sumatera</font>
+</center>
